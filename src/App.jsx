c27051acb286689ee8afc111a8a8eb09b1047e44
Proception <omasan.esimaje@gmail.com>
2019-02-07 23:40:32 +0100

    fix(router): move router to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './components/Routes'
 import Menu from './components/Menu'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Menu/>
-        <Routes/>
-      </div>
+      <Router>
+        <div>
+          <Menu/>
+          <Routes/>
+        </div>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import './assets/main.scss';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+    <App />,
   document.getElementById('app')
 );
