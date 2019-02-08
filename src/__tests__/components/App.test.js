3103ea5028a7f593481897c255584f818ede8dd1
abejide001 <abejidefemi1@gmail.com>
2019-02-08 16:23:44 +0100

    chore(travis): Update travis-ci to run test set up jest and enzyme set up test environment set up travis ci config [Finishes #162727413]

---
+++ src/__tests__/components/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../App';
+
+let wrapper;
+beforeEach(() => {
+ wrapper = shallow(<App />);
+})
+describe('App Component', () => {
+  it('should match snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+  })
+})
