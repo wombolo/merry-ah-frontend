4e813e7f19993d30079b3f58e7dfdfd7872ef01a
abejide001 <abejidefemi1@gmail.com>
2019-02-11 10:26:39 +0100

    fix(feedback): Implement feedback

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,8 +1,8 @@
 import { TEST_DISPATCH } from './types';
 
-export const loginUser = userData => {
-    return {
+export const loginUser = userData => dispatch => {
+    dispatch({
         type: TEST_DISPATCH,
         payload: userData
-    }
+    })
 }
