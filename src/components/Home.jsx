544327e9dd2a2f34c7f88a5f772d41e2cbea3c51
chidimma Ezekwe <julietezekwe@gmail.com>
2019-02-11 14:37:57 +0100

    Chore(Bootstrap setup):Set up  bootstrap css [Chore #163857760]

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 export default class Home extends Component {
     render() {
       return (
-        <div>
+        <div className="container">
           <h1>Home Page</h1>
+          <i className="fa fa-facebook" />
         </div>
       )
     }
