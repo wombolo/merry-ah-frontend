d1eddb67bb8ab87b4c4b07de7bb63fbd35863ba4
Proception <omasan.esimaje@gmail.com>
2019-02-14 16:00:33 +0100

    ft(featured): add featured component

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -3,23 +3,18 @@ import FeatureButton from './FeatureButton';
 
 export default class FeatureCard extends Component {
   render() {
-    const { imgUrl, title
+    const { imgUrl, title, activeClass
     } = this.props;
     return (
-      <div className="item active">
-        <div className="col-xs-4">
-          <div id="cards" style={
-                        {background: `linear-gradient(rgba(0, 0, 0, 0.5), 
-                            rgba(0, 0, 0, 0.5)), url(${imgUrl}) no-repeat center `,
-                        backgroundSize: 'cover'}
-                        }>
-                        <div id="feature_button_div">
-                          <h5>{title}</h5>
-                          <FeatureButton text="View"/>
-                        </div>
+      <div id="cards" className={activeClass} style={
+                    {background: `linear-gradient(rgba(0, 0, 0, 0.5), 
+                        rgba(0, 0, 0, 0.5)), url(${imgUrl}) no-repeat center `,
+                    backgroundSize: 'cover'}
+                    }>
+          <div id="feature_button_div">
+            <h5>{title}</h5>
+            <FeatureButton text="View"/>
           </div>
-          {/* <a href="#"><img src={imgUrl} className="img-responsive"></img></a> */}
-        </div>
       </div>
     )
   }
--- src/components/FeaturedArts.jsx
+++ src/components/FeaturedArts.jsx
@@ -1,36 +1,34 @@
 import React, { Component } from 'react';
 import FeatureCard from './FeatureCard';
+import FeatureButton from './FeatureButton';
+import Arrow from './FeatureArrow';
 
 export default class FeaturedArts extends Component {
   constructor(props) {
     super(props);
     this.state = {
       artList: [
-          {objectID: 1, url:"//placehold.it/400x400?text=1", title: "PAINTING", category:"painting"},
+          {objectID: 1, url:"//placehold.it/400x600?text=1", title: "PAINTING", category:"painting"},
           {objectID: 2, url:"//placehold.it/400x400?text=2", title: "FREEHAND SKETCH", category:"freehand_sketch"},
           {objectID: 3, url:"//placehold.it/400x400?text=3", title: "MOSAIC", category:"mosaic"},
         ],
+      
   };
 }
   render() {
     return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 offset-4 center-block">
-          <div className="carousel slide" id="myCarousel">
-            <div className="carousel-inner">
-              { 
-                this.state.artList.map(
-                    item => <FeatureCard key={item.objectID} title={item.title} imgUrl={item.url}/> 
-                )
-              }
-            </div>
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-          </div>
+        
+        <div id="feature_list">
+          { 
+            this.state.artList.map(
+                (item, index) => <FeatureCard key={item.objectID} activeClass={index === 1 ? "cards_active" : "cards_inactive"} title={item.title} imgUrl={item.url}/> 
+            )
+          }
+          <div id="row">
+          <Arrow className="slide-arrow" direction="left" glyph="&#9664;"/>
+          <Arrow className="slide-arrow" direction="right" glyph="&#9654;"/>
         </div>
-      </div>
-    </div>
+        </div>
     )
   }
 }
