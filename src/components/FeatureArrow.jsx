fecb862d41b5a1433f2c0c4947327e3235eda03c
Proception <omasan.esimaje@gmail.com>
2019-02-18 16:14:50 +0100

    fix(comment): add comment

--- src/components/FeatureArrow.jsx
+++ src/components/FeatureArrow.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Arrow = ({ direction, clickFunction, glyph }) => {
   return (<div className={`slide-arrow ${direction}`} onClick={clickFunction}>
     {glyph}
 </div>);
+};
+
+Arrow.propTypes = {
+  direction: PropTypes.string.isRequired,
+  clickFunction: PropTypes.func.isRequired,
+  glyph: PropTypes.string.isRequired,
 };
 
 export default Arrow;
