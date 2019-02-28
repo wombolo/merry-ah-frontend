9b71febade8dfd29b665ef3b24202fcec20de8e2
Didun Morodolu <elkenzom1@gmail.com>
2019-02-28 19:36:13 +0100

    merge conflict

--- src/reducers/artReducer.js
+++ src/reducers/artReducer.js
@@ -1,16 +1,20 @@
 import {
-  SET_USER_REQUEST,GET_CATEGORIES_FILES, SET_FILE_ERROR
+  GET_CATEGORIES_FILES, SET_FILE_ERROR,
 } from '../actions/types';
 
 const initialState = {
-  categories:[],
-  error: ''
+  categories: [],
+  error: '',
 };
 
-
+/**
+ * Arts Reducer
+ * @param {object} state
+ * @param {object} action
+ * @returns {object} state
+ */
 function artReducer(state = initialState, action) {
   switch (action.type) {
-
     case GET_CATEGORIES_FILES:
       return {
         ...state,
@@ -19,12 +23,10 @@ function artReducer(state = initialState, action) {
     case SET_FILE_ERROR:
       return {
         ...state,
-        error: action.payload
+        error: action.payload,
       };
     default:
       return state;
   }
 }
 export default artReducer;
-
-
