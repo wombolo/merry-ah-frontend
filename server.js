b195834348c99a3c12e1cf11930a3e6d3374f2d2
Didun Morodolu <elkenzom1@gmail.com>
2019-03-28 14:41:25 +0100

    Set default port to 5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static(`${__dirname}/dist/`));
