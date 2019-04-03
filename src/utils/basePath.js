192f7b29f34d586097a100232551e6d5abb1e3f8
Didun Morodolu <elkenzom1@gmail.com>
2019-04-03 20:51:02 +0100

    Allow BACKEND_API be modified .env

--- src/utils/basePath.js
+++ src/utils/basePath.js
@@ -1,2 +1,7 @@
 // export const basePath = 'http://localhost:9000/api/v1';
-export const basePath = 'https://merry-ah-staging.herokuapp.com/api/v1';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const { BACKEND_API } = process.env;
+
+export const basePath = BACKEND_API;
