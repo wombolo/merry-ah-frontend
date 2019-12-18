42074db370371f0b149933127d9478325a86d506
wombolo <elkenzom1@gmail.com>
2019-12-18 09:49:55 +0100

    --amend

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,16 +1,23 @@
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
 
 
 module.exports = merge(common, {
+  node: {
+    fs: "empty"
+  },
   mode: 'production',
   devtool: 'source-map',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        sourceMap: true,
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
       }),
       new OptimizeCSSAssetsPlugin({})
     ],
